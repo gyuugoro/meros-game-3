a751eb4b153e55f71ab6a8cf915ca471f91adda4
Tenohira <tenohira.taro@gmail.com>
2021-02-07 17:06:03 +0900

    Home画面作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>Home</h1>
+  <v-container class="mx-auto my-auto">
+    <v-form ref="input">
+      <v-text-field
+        label="あなたのあだ名"
+        outlined
+        color="purple"
+        autofocus
+        append-icon="mdi-account-arrow-right"
+        :rules="[rule]"
+        @click:append="start()"
+        @keypress.enter.prevent
+        @keypress.enter="start()"
+      ></v-text-field>
+    </v-form>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      rule: (v) => !!v || "ちゃんと入力！",
+    };
+  },
+  methods: {
+    start() {
+      if (this.$refs.input.validate()) {
+        console.log("start");
+      }
+    },
+  },
+};
+</script>
